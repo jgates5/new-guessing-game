0f59c17a484a9b68cda074ece0fa8460511b52d9
jeffwgates <jeff.gates0507@gmail.com>
2020-06-05 09:02:38 -0600

    New file for Guessing Game Project

---
+++ game-guess.js
@@ -0,0 +1,16 @@
+let winnings = 0
+
+console.log ("welcome to Jeff's Keno shortend version guessing game.\n\n\n")
+console.log("Here are the Rules:\n\n")
+console.log("1. Takes in 1.00 to play.\n")
+console.log("2. To start you will be asked to select 5 random numbers between 1 and 80.\n")
+console.log("3. Then, 10 random numbers will be selected from that numbered amount\n")
+console.log("4. You will win 2.00 per number you guess right. If you guess wrong you lose 2.00 per number. IF you guess all the numbers correctly you win 100.00.\n\n\n")
+
+function gambleAmount(Number) {
+  for (let item in Number) {
+    if(random number[Num] === "number") {
+      console.log()
+    }
+  }
+}
