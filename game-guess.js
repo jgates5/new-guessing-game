31c32421db35900879a0011d494ce4a98722b04e
jeffwgates <jeff.gates0507@gmail.com>
2020-06-11 10:33:18 -0600

    updated final project with while loop to restart program for new game

--- game-guess.js
+++ game-guess.js
@@ -1,73 +1,83 @@
-let winnings = 0
-let total = 0
-var userGuessArr = []
+let total = 10
+	 
+const userGuess = []
+const matchNumArray = []
+forRandomNumberArray = [1, 2, 3, 4, 5]
+
 console.log ("welcome to Jeff's Keno shortend version guessing game.\n\n\n")
 console.log("Here are the Rules:\n\n")
-console.log("1. Takes in 1.00 to play.\n")
-console.log("2. To start you will be asked to select 5 random numbers between 1 and 80.\n")
-console.log("3. Then, 10 random numbers will be selected from that numbered amount\n")
-console.log("4. You will win 2.00 per number you guess right. If you guess wrong you lose 2.00 per number. IF you guess all the numbers correctly you win 100.00.\n\n\n")
+console.log("1. Takes in $10.00 to play.\n")
+console.log("2. To start you will be asked to select 5 random numbers between 1 and 5.\n")
+console.log("3. Then, 5 random numbers will be selected from that numbered amount\n")
+console.log("4. You will win 20.00 per number you guess right. If you guess wrong you lose 20.00 per number.\n\n\n")
+
+while(true)(
+
 
 function num() {
 const betnum = prompt("Pick your bet!");
-  if((betnum >= 1) && (betnum <= 5) ) {
-      alert("Let the game begin!");
-      userGuess()
-   total += betnum
-  } else if (betnum < 1) {
-      alert("That is not an option, Please pick a new number.");
-  } else if (betnum >= 51) {
-      alert("That is not an option, Please pick a new number.");
-      num();
-  } 
+if((betnum >= 0) && (betnum <= 10) ) {
+    alert("Let the game begin!");
+   total = total - 10.00;
+} else if (betnum < 1) {
+    alert("That is not an option, Please pick a new number.");
+} else if (betnum >= 11) {
+   alert("That is not an option, Please pick a new number.");
+num();
+  
+ } 
+}
+num();
+
+
+
+
+for ( let i = 0; i < 5; i++) {              
+userGuess.push(prompt('Enter number between 1 and 5 '));
+userGuess.push(prompt('Enter number between 1 and 5 '));
+userGuess.push(prompt('Enter number between 1 and 5 '));
+userGuess.push(prompt('Enter number between 1 and 5 '));
+userGuess.push(prompt('Enter number between 1 and 5 '));
+total= total + 20;
+
+alert('Your guess of numbers: ' + userGuess.join(' '));
+
+const randomMaker = () => {
+for (let i =0; i < 5; i++ ) {
+matchNumArray.push(forRandomNumberArray[Math.floor(Math.random()*forRandomNumberArray.length)])
+ total = total +- 20;
+}
+}
+randomMaker()
+
+
+console.log(prompt('Here is the random guess number: ' + matchNumArray.join(" ")));
+
+
+function winLottery(userGuess, matchNumArray) {
+let message = "You Lose";
+for (let i = 0; i < userGuess.length; i++ ) {
+for (let j = 0; j < matchNumArray.length; j++ ) {
+if(userGuess[i] == matchNumArray[j]) {
+message = "You Win";
+alert(message)
+total = total +- 20;
+}
+}
+}
+}
+
+winLottery(userGuess, matchNumArray);
+  alert("Your new total Wallet is " +- total);
+
+const playAgain = prompt('Would you like to play again?');
+if('playAgain === yes') {
+alert("Awesome!");
+} else {
+alert('Thanks for playing.')
+break;
 }
 num()
-
-function userGuess() {
-  let count = 1;
-  while (count < 6) {
-    count +=1;
-
-
-  const guess = prompt("Enter a number between 1 and 9");
-  if (guess > 0 && guess <10) {
-    userGuessArr.push(guess)
-   } else if (guess !== null) {
-    alert ('That is not an option Please try again!')
-    userGuess();
-    total - 1 
-    break
-   }
-  }
-
-}
-alert('Here are your guessed numbers?')
- let randomNum = userGuessArr[(Math.random() * userGuessArr.length)]
- 
-console.log(userGuessArr.join(" "))
-
-alert('Here are the numbers that are randomly picked?')
-
-const randomNumber = ['1', '2', '3', '4', '5']; 
-
-const emptyInt = []
-
-for(let i = 0; i < randomNumber.length; i++){
-  emptyInt.push(Math.floor(Math.random() * 10));
-
-
 }
 
-console.log(emptyInt.join(" "));
-
-
-var userGuestArr = prompt("Congrats you won!");
-if (userGuessArr > 1 && userGuess < 9){
-  alert `You earned this $${userGuestArr}`
-   total = total +- emptyInt
-} else if (userGuestArr !== null) {
-  alert('Sorry, you lost!')
-}
-
- alert('Here is your total earnings plus money betted')
-  alert(total);
+  )
